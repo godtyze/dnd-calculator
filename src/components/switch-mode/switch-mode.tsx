347f36eb52221be14bf23ed704834c6a6efd56ea
godtyze <omgfail301@gmail.com>
2023-03-12 15:53:14 +0600

    feat: add styles for switch buttons active

--- src/components/switch-mode/switch-mode.tsx
+++ src/components/switch-mode/switch-mode.tsx
@@ -2,11 +2,11 @@ import React, {useCallback} from 'react';
 import {ReactComponent as Eye} from '../../assets/eye.svg';
 import {ReactComponent as Vector} from '../../assets/Vector.svg';
 import Button from '../UI/button/button';
-import './switch-mode.scss';
 import {isConstructorMode} from '../../utils';
 import {useAppSelector} from '../../hooks/redux';
 import {selectMode} from '../../store/slices/calculatorSlice';
 import {useActions} from '../../hooks/useActions';
+import './switch-mode.scss';
 
 const SwitchMode: React.FC = () => {
   const mode = useAppSelector(selectMode);
@@ -27,14 +27,14 @@ const SwitchMode: React.FC = () => {
         className={isConstructorMode(mode) ? 'switch-mode__btn' : 'switch-mode__btn active'}
         onClick={onClickRuntime}
       >
-        <Eye/>
+        <Eye className={isConstructorMode(mode) ? '' : 'active'}/>
         <span>Runtime</span>
       </Button>
       <Button
         className={isConstructorMode(mode) ? 'switch-mode__btn active' : 'switch-mode__btn'}
         onClick={onClickConstructor}
       >
-        <Vector/>
+        <Vector className={isConstructorMode(mode) ? 'active' : ''}/>
         <span>Constructor</span>
       </Button>
     </div>
